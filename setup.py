7a33c689cf9ea85f45c7ae94a91ab5181044371a
Beigh-Farooq <beighfarooq199@gmail.com>
2025-12-14 22:33:05 +0530

    Created Folder Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name = 'Generative AI Project',
+    version = '0.0.0',
+    author = 'Beigh Farooq',
+    author_email = 'beighfarooq199@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
